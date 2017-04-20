8c53a651163d2291d41703cd385c678cdb7c698a
vizarch <krzysztof.luczak99@gmail.com>
2017-04-20 19:38:53 +0200

    poprawka htmli

--- webscraper/main_app/views.py
+++ webscraper/main_app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def start_page(request):
-    return render(request, 'main_app/Home.html')
+    if request.user.is_authenticated:
+        # jest zalogowany
+        return render(request, 'main_app/Home_4_logged.html')
+    else:
+        # NIE jest zalogowany
+        return render(request, 'main_app/Home_4_notLogged.html')
