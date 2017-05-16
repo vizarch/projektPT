6654d4046cc2a4057b95ff8b4883a874f0c40442
DawidWiktor <dawidnfs@gmail.com>
2017-05-16 12:51:00 +0200

    zmiana dat

--- webscraper/main_app/scrapers/altcontroldelete.py
+++ webscraper/main_app/scrapers/altcontroldelete.py
@@ -13,18 +13,18 @@ END = False
 def month2int(month):
     month = month.lower()
     months = {
-    "JAN": '01',
-    "FEB": '02',
-    "MAR": '03',
-    "APR": '04',
-    "MAY": '05',
-    "JUN": '06',
-    "JUL": '07',
-    "AUG": '08',
-    "SEP": '09',
-    "OCT": '10',
-    "NOV": '11',
-    "DEC": '12'
+    "Jan": '01',
+    "Feb": '02',
+    "Mar": '03',
+    "Apr": '04',
+    "May": '05',
+    "Jun": '06',
+    "Jul": '07',
+    "Aug": '08',
+    "Sep": '09',
+    "Oct": '10',
+    "Nov": '11',
+    "Dec": '12'
     }
     return months.get(month)
 
